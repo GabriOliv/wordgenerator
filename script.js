3c3cc9092512fa3e75a95441389213ef11e72da4
gov <gabcomp.gov@gmail.com>
2022-05-02 15:21:53 -0300

    v0.1.1
    refactor
    feat
        add salt
        clear only salt
        clear only key

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
-// Main Button
+// # Main Menu
+
+// ## Function Cliboard
 function clip_board(copy_text){
 	//Create Element
 	const Temporary = document.createElement('textarea');
@@ -15,29 +17,65 @@ function clip_board(copy_text){
 	document.body.removeChild(Temporary);
 }
 
-function generate_hash(){
-	var str = document.getElementById("keyword").value;
+// ## Function Salt Word Generate
+function salt_word(){
+	let result = '';
+	let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	let charactersLength = 26;
+	
+	for (let i = 0; i < 10; i++){
+		result += alphabet.charAt(Math.floor(Math.random() * 26));
+	}
+	return result;
+}
 
-	var SHA256 = new Hashes.SHA256().hex(str);
-	var SHA512 = new Hashes.SHA512().hex(str);
-	var HASHMD5 = new Hashes.MD5().hex(str);
+// ## Button Generate
+function generate_hash(){
+	let key_str = document.getElementById("keyword").value;
+	let salt_str = document.getElementById("salt").value;
+
+	//Keyword String + Salt String
+	key_str += salt_str;
+
+	let SHA256 = new Hashes.SHA256().hex(key_str);
+	let SHA512 = new Hashes.SHA512().hex(key_str);
+	let HASHMD5 = new Hashes.MD5().hex(key_str);
 
 	document.getElementById("output_password_256").value = SHA256;
 	document.getElementById("output_password_512").value = SHA512;
 	document.getElementById("output_password_MD5").value = HASHMD5;
 }
 
+// ## Button Random Key
 function random_key(){
-
-	var SHA256 = new Hashes.SHA512().hex(Math.random());
+	//generate Keyword
+	let SHA256 = new Hashes.SHA512().hex(Math.random());
 	document.getElementById("keyword").value = SHA256;
+
+	//generate Salt
+	let salt_str = salt_word();
+	document.getElementById("salt").value = salt_str;
 }
 
+// ## Button Clear Key Input
+function clear_keyword(){
+	let str = "";
+	document.getElementById("keyword").value = str;
+}
+
+// ## Button Clear Salt Input
+function clear_salt(){
+	let str = "";
+	document.getElementById("salt").value = str;
+}
+
+// ## Button Clear Keys
 function clear_all(){
-	var str = "";
+	let str = "";
 	clip_board("_");
 
 	document.getElementById("keyword").value = str;
+	document.getElementById("salt").value = str;
 	document.getElementById("output_password_256").value = str;
 	document.getElementById("output_password_512").value = str;
 	document.getElementById("output_password_MD5").value = str;
@@ -45,14 +83,16 @@ function clear_all(){
 
 
 
-// 256
+// # SHA256 Menu
+
+// ## Button Copy 256 All
 function copy_256_all(){
 	//Get Element to x
-	var x = document.getElementById("output_password_256");
+	let x = document.getElementById("output_password_256");
 	// Make Password -> Text
 	x.type = "text";
 
-	var copyText = document.getElementById("output_password_256");
+	let copyText = document.getElementById("output_password_256");
 	copyText.select();
 	document.execCommand("copy");
 
@@ -60,15 +100,16 @@ function copy_256_all(){
 	x.type = "password";
 }
 
+// ## Button Copy 256 First Chars
 function copy_256_f(size){
 	//Get Element to x
-	var x = document.getElementById("output_password_256");
+	let x = document.getElementById("output_password_256");
 	// Make Password -> Text
 	x.type = "text";
 	//Copy Value to String
-	var copyText = document.getElementById("output_password_256").value;
+	let copyText = document.getElementById("output_password_256").value;
 	//Slice First
-	var copyText = copyText.slice(0, size);
+	copyText = copyText.slice(0, size);
 
 	clip_board(copyText);
 
@@ -76,19 +117,20 @@ function copy_256_f(size){
 	x.type = "password";
 }
 
+// ## Button Copy 256 Last Chars
 function copy_256_l(size){
 	//Get Element to x
-	var x = document.getElementById("output_password_256");
+	let x = document.getElementById("output_password_256");
 	// Make Password -> Text
 	x.type = "text";
 	//Copy Value to String
-	var copyText = document.getElementById("output_password_256").value;
+	let copyText = document.getElementById("output_password_256").value;
 	//Save Size of String
-	var copy_length = copyText.length;
+	let copy_length = copyText.length;
 	//Create a Start of Slice
-	var copy_start = copy_length - size;
+	let copy_start = copy_length - size;
 	//Slice Last
-	var copyText = copyText.slice(copy_start, copy_length);
+	copyText = copyText.slice(copy_start, copy_length);
 
 	clip_board(copyText);
 
@@ -98,14 +140,16 @@ function copy_256_l(size){
 
 
 
-// 512
+// # SHA512 Menu
+
+// ## Button Copy 512 All
 function copy_512_all(){
 	//Get Element to x
-	var x = document.getElementById("output_password_512");
+	let x = document.getElementById("output_password_512");
 	// Make Password -> Text
 	x.type = "text";
 
-	var copyText = document.getElementById("output_password_512");
+	let copyText = document.getElementById("output_password_512");
 	copyText.select();
 	document.execCommand("copy");
 
@@ -113,15 +157,16 @@ function copy_512_all(){
 	x.type = "password";
 }
 
+// ## Button Copy 512 First Chars
 function copy_512_f(size){
 	//Get Element to x
-	var x = document.getElementById("output_password_512");
+	let x = document.getElementById("output_password_512");
 	// Make Password -> Text
 	x.type = "text";
 	// Copy Value to String
-	var copyText = document.getElementById("output_password_512").value;
+	let copyText = document.getElementById("output_password_512").value;
 	// Slice First
-	var copyText = copyText.slice(0, size);
+	copyText = copyText.slice(0, size);
 
 	clip_board(copyText);
 
@@ -129,19 +174,20 @@ function copy_512_f(size){
 	x.type = "password";
 }
 
+// ## Button Copy 512 Last Chars
 function copy_512_l(size){
 	//Get Element to x
-	var x = document.getElementById("output_password_512");
+	let x = document.getElementById("output_password_512");
 	// Make Password -> Text
 	x.type = "text";
 	//Copy Value to String
-	var copyText = document.getElementById("output_password_512").value;
+	let copyText = document.getElementById("output_password_512").value;
 	//Save Size of String
-	var copy_length = copyText.length;
+	let copy_length = copyText.length;
 	//Create a Start of Slice
-	var copy_start = copy_length - size;
+	let copy_start = copy_length - size;
 	//Slice Last
-	var copyText = copyText.slice(copy_start, copy_length);
+	copyText = copyText.slice(copy_start, copy_length);
 
 	clip_board(copyText);
 
@@ -151,14 +197,16 @@ function copy_512_l(size){
 
 
 
-// MD5
+// # MD5 Menu
+
+// ## Button Copy MD5 All
 function copy_MD5_all(){
 	//Get Element to x
-	var x = document.getElementById("output_password_MD5");
+	let x = document.getElementById("output_password_MD5");
 	// Make Password -> Text
 	x.type = "text";
 
-	var copyText = document.getElementById("output_password_MD5");
+	let copyText = document.getElementById("output_password_MD5");
 	copyText.select();
 	document.execCommand("copy");
 
@@ -166,15 +214,16 @@ function copy_MD5_all(){
 	x.type = "password";
 }
 
+// ## Button Copy MD5 First Chars
 function copy_MD5_f(size){
 	//Get Element to x
-	var x = document.getElementById("output_password_MD5");
+	let x = document.getElementById("output_password_MD5");
 	// Make Password -> Text
 	x.type = "text";
 	// Copy Value to String
-	var copyText = document.getElementById("output_password_MD5").value;
+	let copyText = document.getElementById("output_password_MD5").value;
 	// Slice First
-	var copyText = copyText.slice(0, size);
+	copyText = copyText.slice(0, size);
 
 	clip_board(copyText);
 
@@ -182,19 +231,20 @@ function copy_MD5_f(size){
 	x.type = "password";
 }
 
+// ## Button Copy MD5 Last Chars
 function copy_MD5_l(size){
 	//Get Element to x
-	var x = document.getElementById("output_password_MD5");
+	let x = document.getElementById("output_password_MD5");
 	// Make Password -> Text
 	x.type = "text";
 	//Copy Value to String
-	var copyText = document.getElementById("output_password_MD5").value;
+	let copyText = document.getElementById("output_password_MD5").value;
 	//Save Size of String
-	var copy_length = copyText.length;
+	let copy_length = copyText.length;
 	//Create a Start of Slice
-	var copy_start = copy_length - size;
+	let copy_start = copy_length - size;
 	//Slice Last
-	var copyText = copyText.slice(copy_start, copy_length);
+	copyText = copyText.slice(copy_start, copy_length);
 
 	clip_board(copyText);
 
@@ -204,26 +254,28 @@ function copy_MD5_l(size){
 
 
 
-// Re-Hash
+// # Re-Hash
+
+// ## Button Re-Use HASH SHA256
 function re_generate_hash_256(){
-	var str = document.getElementById("output_password_256").value;
+	let str = document.getElementById("output_password_256").value;
 	document.getElementById("keyword").value = str;
 
 	generate_hash();
 }
 
+// ## Button Re-Use HASH SHA512
 function re_generate_hash_512(){
-	var str = document.getElementById("output_password_512").value;
+	let str = document.getElementById("output_password_512").value;
 	document.getElementById("keyword").value = str;
 
 	generate_hash();
 }
 
+// ## Button Re-Use HASH MD5
 function re_generate_hash_MD5(){
-	var str = document.getElementById("output_password_MD5").value;
+	let str = document.getElementById("output_password_MD5").value;
 	document.getElementById("keyword").value = str;
 
 	generate_hash();
 }
-
-
